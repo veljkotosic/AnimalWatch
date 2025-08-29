890d50a2b63385fad34b5cf4f674426cee6d1728
Veljko Tosic <veljkotosic6@gmail.com>
2025-08-29 11:31:02 +0200

    Odvojena glavna aktivnost od autorizacije

--- app/src/main/java/com/veljkotosic/animalwatch/activity/HomeActivity.kt
+++ app/src/main/java/com/veljkotosic/animalwatch/activity/HomeActivity.kt
@@ -6,25 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import com.veljkotosic.animalwatch.data.auth.repository.FireBaseAuthRepository
+import androidx.compose.animation.ExperimentalAnimationApi
 import com.veljkotosic.animalwatch.data.storage.CloudinaryStorageRepository
 import com.veljkotosic.animalwatch.data.user.repository.FirestoreUserRepository
-import com.veljkotosic.animalwatch.navigation.auth.AuthNavHost
+import com.veljkotosic.animalwatch.navigation.home.HomeNavHost
 import com.veljkotosic.animalwatch.ui.theme.AnimalWatchTheme
-import com.veljkotosic.animalwatch.viewmodel.auth.AuthViewModel
-import com.veljkotosic.animalwatch.viewmodel.auth.AuthViewModelFactory
-import com.veljkotosic.animalwatch.viewmodel.auth.LoginViewModel
-import com.veljkotosic.animalwatch.viewmodel.auth.RegistrationViewModel
 import com.veljkotosic.animalwatch.viewmodel.user.UserViewModel
 import com.veljkotosic.animalwatch.viewmodel.user.UserViewModelFactory
 import kotlin.getValue
 
-class AuthActivity : ComponentActivity() {
-    private val authViewModelFactory = AuthViewModelFactory(FireBaseAuthRepository())
-
-    private val loginViewModel: LoginViewModel by viewModels { authViewModelFactory }
-    private val registrationViewModel: RegistrationViewModel by viewModels { authViewModelFactory }
-
+class HomeActivity : ComponentActivity() {
     private val userViewModel: UserViewModel by viewModels {
         UserViewModelFactory(
             FirestoreUserRepository(),
@@ -34,20 +25,14 @@ class AuthActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val signedOut = intent.getBooleanExtra("signedOut", false)
-
         enableEdgeToEdge()
-
         setContent {
             AnimalWatchTheme {
-                AuthNavHost(
-                    signedOut = signedOut,
-                    loginViewModel = loginViewModel,
-                    registrationViewModel = registrationViewModel,
+                HomeNavHost(
                     userViewModel = userViewModel,
-                    onLoginSuccess = {
-                        val intent = Intent(this, HomeActivity::class.java)
+                    onSignOut = {
+                        val intent = Intent(this, AuthActivity::class.java)
+                        intent.putExtra("signedOut", true)
                         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         startActivity(intent)
                         finish()
