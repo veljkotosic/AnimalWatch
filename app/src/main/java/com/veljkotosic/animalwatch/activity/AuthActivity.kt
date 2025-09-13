44c2fbea4f58fc9c058b21ed65f89ca300cc334b
Veljko Tosic <veljkotosic6@gmail.com>
2025-09-13 18:40:06 +0200

    Provera dal je ukljucen internet

--- app/src/main/java/com/veljkotosic/animalwatch/activity/AuthActivity.kt
+++ app/src/main/java/com/veljkotosic/animalwatch/activity/AuthActivity.kt
@@ -1,6 +1,9 @@
 package com.veljkotosic.animalwatch.activity
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,14 +14,37 @@ import com.veljkotosic.animalwatch.data.storage.CloudinaryStorageRepository
 import com.veljkotosic.animalwatch.data.user.repository.FirestoreUserRepository
 import com.veljkotosic.animalwatch.navigation.auth.AuthNavHost
 import com.veljkotosic.animalwatch.ui.theme.AnimalWatchTheme
+import com.veljkotosic.animalwatch.utility.service.isInternetConnectionEnabled
 import com.veljkotosic.animalwatch.viewmodel.auth.AuthViewModelFactory
 import com.veljkotosic.animalwatch.viewmodel.auth.LoginViewModel
 import com.veljkotosic.animalwatch.viewmodel.auth.RegistrationViewModel
 import com.veljkotosic.animalwatch.viewmodel.user.UserViewModel
 import com.veljkotosic.animalwatch.viewmodel.user.UserViewModelFactory
-import kotlin.getValue
+import kotlin.system.exitProcess
 
 class AuthActivity : ComponentActivity() {
+    private val connectionManager by lazy {
+        getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onLost(network: Network) {
+            showExitDialog("Internet connection is required for the app to function properly")
+        }
+    }
+
+    private fun showExitDialog(message: String) {
+        AlertDialog.Builder(this)
+            .setTitle("App closing")
+            .setMessage(message)
+            .setCancelable(false)
+            .setPositiveButton("OK") { _, _ ->
+                finishAffinity()
+                exitProcess(0)
+            }
+            .show()
+    }
+
     private val authViewModelFactory = AuthViewModelFactory(FireBaseAuthRepository())
 
     private val loginViewModel: LoginViewModel by viewModels { authViewModelFactory }
@@ -52,6 +78,26 @@ class AuthActivity : ComponentActivity() {
                         finish()
                     })
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        connectionManager.registerDefaultNetworkCallback(networkCallback)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        connectionManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (!isInternetConnectionEnabled(this)) {
+            showExitDialog("Internet connection is required for the app to function properly")
         }
     }
 }
