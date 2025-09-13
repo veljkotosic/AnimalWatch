3522a7698a13168cdc5ef869001b52e58f4ccb14
Veljko Tosic <veljkotosic6@gmail.com>
2025-09-13 17:59:51 +0200

    Entitet statistike (za rangiranje korisnika)

---
+++ app/src/main/java/com/veljkotosic/animalwatch/data/stat/entity/UserStats.kt
@@ -0,0 +1,7 @@
+package com.veljkotosic.animalwatch.data.stat.entity
+
+data class UserStats(
+    val markersCreatedCount: Int = 0,
+    val markersUpdatedCount: Int = 0,
+    val totalAppraisals: Int = 0
+)
