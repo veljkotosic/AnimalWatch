4acd5b8351aaf26c7dcbe67001da177af31850e3
Veljko Tosic <veljkotosic6@gmail.com>
2025-09-25 19:18:20 +0200

    Za work manager (zapravo ga ne koristim)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.foundation)
+    implementation(libs.androidx.work.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
