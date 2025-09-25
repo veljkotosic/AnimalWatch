a82a53af58cad73414468609a534401e8ef3c545
Veljko Tosic <veljkotosic6@gmail.com>
2025-09-25 22:42:54 +0200

    Dodata nova polja

--- app/src/main/java/com/veljkotosic/animalwatch/data/stat/entity/UserStats.kt
+++ app/src/main/java/com/veljkotosic/animalwatch/data/stat/entity/UserStats.kt
@@ -1,7 +1,9 @@
 package com.veljkotosic.animalwatch.data.stat.entity
 
 data class UserStats(
+    val username: String = "",
     val markersCreatedCount: Int = 0,
     val markersUpdatedCount: Int = 0,
-    val totalAppraisals: Int = 0
+    val totalAppraisals: Int = 0,
+    val total: Int = 0
 )
