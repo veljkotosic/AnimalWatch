4761c186668f6c3154bd68959ddf50aa95308379
Veljko Tosic <veljkotosic6@gmail.com>
2025-10-22 00:26:42 +0200

    Osnovno za profil screen

--- app/src/main/java/com/veljkotosic/animalwatch/screen/home/ProfileScreen.kt
+++ app/src/main/java/com/veljkotosic/animalwatch/screen/home/ProfileScreen.kt
@@ -1,11 +1,10 @@
-@file:JvmName("ProfileScreenKt")
-
 package com.veljkotosic.animalwatch.screen.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -14,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.veljkotosic.animalwatch.screen.Screens
+import com.veljkotosic.animalwatch.ui.theme.AnimalWatchTheme
 import com.veljkotosic.animalwatch.viewmodel.profile.ProfileViewModel
 
 @Composable
@@ -28,7 +28,8 @@ fun ProfileScreen(
     ) {
         Text(
             "Home screen!",
-            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+            style = MaterialTheme.typography.headlineMedium
         )
 
         TextButton(
