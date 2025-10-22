2ce9590f0082e6ca81e482f44b7b891cfb9773c8
Veljko Tosic <veljkotosic6@gmail.com>
2025-10-22 22:46:54 +0200

    Basic lending pejdz

--- app/src/main/java/com/veljkotosic/animalwatch/viewmodel/profile/ProfileViewModelFactory.kt
+++ app/src/main/java/com/veljkotosic/animalwatch/viewmodel/profile/ProfileViewModelFactory.kt
@@ -3,14 +3,16 @@ package com.veljkotosic.animalwatch.viewmodel.profile
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.veljkotosic.animalwatch.data.auth.repository.AuthRepository
+import com.veljkotosic.animalwatch.data.user.repository.UserRepository
 
 class ProfileViewModelFactory(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository
 ) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
-            return ProfileViewModel(authRepository) as T
+            return ProfileViewModel(authRepository, userRepository) as T
         } else {
             throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
